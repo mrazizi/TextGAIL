d8a9f0defdb1c7d1b16071fab1fba77effaed29d
Mohammad-Reza Azizi <mreza.azizi74@gmail.com>
2021-11-22 19:13:16 +0330

    merge latest torchfly utilities folder

---
+++ TorchFly/torchfly/utilities/__init__.py
@@ -0,0 +1,5 @@
+from .async_save import async_save, async_wait
+from .move_to_device import move_to_device
+from .logging_util import configure_logging
+from .random_seeding import set_random_seed
+from .get_rank import get_rank
